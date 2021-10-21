c4f32e3a218aafb021de3f2c65cd19841dae9b03
asdfghjklmum <chenqi63656@gmail.com>
2021-10-21 17:56:49 +0800

    test case added and test script added and coded

---
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        float result = 0f;
+
+        [Test]
+        public void TestAddition()
+        {
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+        [UnityTest]
+        public IEnumerator TestSuiteAddition()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+        [UnityTest]
+        public IEnumerator TestSuiteSubtraction()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
+        [UnityTest]
+        public IEnumerator TestSuiteDivision()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
+    }
+}
